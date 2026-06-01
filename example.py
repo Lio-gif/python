0ed6a68d6cc5a876d973db304e41d3403169c32b
Lio-gif <149175168+Lio-gif@users.noreply.github.com>
2026-06-01 23:34:40 +0200

    Change Telegram chat ID and token
    Updated Telegram chat ID and API token for notifications.

--- example.py
+++ example.py
@@ -43,8 +43,8 @@ twitch_miner = TwitchChannelPointsMiner(
             BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
         ),
         telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
-            chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
-            token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
+            chat_id=-1003770581491,                                                      # Chat ID to send messages @getmyid_bot
+            token="8369214340:AAG_YKx4AL3_bM2VPsxfGTiVFNHk2pV7hKU",                          # Telegram API token @BotFather
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.BET_LOSE, Events.CHAT_MENTION],                          # Only these events will be sent to the chat
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
